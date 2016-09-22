221338b5ae11a07374d4422dbd068a34850ddc04
abtevrythng <g.nelson@hia.com.au>
2016-09-23 09:27:42 +1000

    Updated Release Build
    Modified Project details

--- FetchXML2SQLv2/Properties/AssemblyInfo.cs
+++ FetchXML2SQLv2/Properties/AssemblyInfo.cs
@@ -1,31 +1,31 @@
-﻿#region Using directives
-
+#region Using directives
 using System;
 using System.Reflection;
+using System.Resources;
 using System.Runtime.InteropServices;
 
 #endregion
-
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FetchXML2SQLv2")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FetchXML2SQLv2")]
-[assembly: AssemblyCopyright("Copyright 2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
+[assembly: AssemblyTitle ("FetchXML2SQLv2")]
+[assembly: AssemblyDescription ("Convert Dynamics CRM fetchXML to SQL queries")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("abtevrythng")]
+[assembly: AssemblyProduct ("FetchXML2SQLv2")]
+[assembly: AssemblyCopyright ("Copyright 2016")]
+[assembly: AssemblyTrademark ("abtevrythng")]
+[assembly: AssemblyCulture ("")]
 // This sets the default COM visibility of types in the assembly to invisible.
 // If you need to expose a type to COM, use [ComVisible(true)] on that type.
-[assembly: ComVisible(false)]
-
+[assembly: ComVisible (false)]
 // The assembly version has following format :
 //
 // Major.Minor.Build.Revision
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion ("1.0.*")]
+[assembly: AssemblyFileVersion ("2.0.0.0")]
+[assembly: Guid ("9f002210-da44-4ab2-9a8c-e2b727ffb80e")]
+[assembly: NeutralResourcesLanguage ("en")]
